e3e73bb01982a4189139a090c33f5e185e840bff
Prejitha CT <142784014+PrejithaCT@users.noreply.github.com>
2026-08-21 22:37:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,70 @@ SalesforceInteractions.init({
     ]
 })
 .then(() => {
+
     console.log("Salesforce SDK initialized");
+
+    const productButtons =
+        document.querySelectorAll(".product-click");
+
+    productButtons.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            const productId =
+                button.dataset.productId;
+
+            const productName =
+                button.dataset.productName;
+
+            const productCategory =
+                button.dataset.productCategory;
+
+            console.log("PRODUCT CLICKED:", {
+                productId,
+                productName,
+                productCategory
+            });
+
+            SalesforceInteractions.sendEvent({
+
+                interaction: {
+
+                    name:
+                        SalesforceInteractions
+                            .CatalogObjectInteractionName
+                            .ViewCatalogObject,
+
+                    catalogObject: {
+
+                        type: "Product",
+
+                        id: productId,
+
+                        attributes: {
+
+                            name: productName,
+
+                            category: productCategory
+
+                        }
+
+                    }
+
+                }
+
+            });
+
+        });
+
+    });
+
 })
-.catch((error) => {
-    console.error("Salesforce SDK initialization failed:", error);
+.catch(error => {
+
+    console.error(
+        "Salesforce SDK initialization failed:",
+        error
+    );
+
 });
